0cce19ea5eceaff6fd7d9a000c5767443d7fba29
Vu Kim Phuc <91018812+phucoscar@users.noreply.github.com>
2024-09-02 16:15:23 +0700

    Update CmsApiServiceApplication.java

--- src/main/java/com/cms/CmsApiServiceApplication.java
+++ src/main/java/com/cms/CmsApiServiceApplication.java
@@ -1,13 +1,1 @@
-package com.cms;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CmsApiServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CmsApiServiceApplication.class, args);
-	}
-
-}
+hello guy
